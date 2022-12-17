3b80998bd08314b72297d3a8344122d82235f13e
rasheedawilson12 <rasheedawilson12@gmail.com>
2022-12-16 19:36:39 -0600

    added bookstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Cedra Wilson and is{" "}
+          <a
+            href="https://github.com/rasheedawilson12/shecodes-react-weather-app.git"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+          .
+        </footer>
+      </div>
+    </div>
+  );
 }
